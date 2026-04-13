2d828e170537aced1703f545af51a7d3281c6982
Leetokhunou <ST10487651@rcconnect.edu.za>
2026-04-13 20:04:30 +0200

    Add files via upload
    Commit 3 - Improved UI header design

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Chatbot bot = new Chatbot();
+        bot.Start();
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
